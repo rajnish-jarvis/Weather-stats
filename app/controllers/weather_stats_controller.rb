876de983226a93b0d491ffc8d5b866465f5b2254
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2022-08-01 09:41:41 +0530

    Minor changes

--- app/controllers/weather_stats_controller.rb
+++ app/controllers/weather_stats_controller.rb
@@ -2,6 +2,10 @@ class WeatherStatsController < ApplicationController
   include ApplicationConcern
   # Get Weather stats of next five days with a city
   def index
+    begin
     get_weather_stats(params)
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
   end
 end
